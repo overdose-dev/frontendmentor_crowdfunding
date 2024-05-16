099681e6a56802ca2ee76273bc99f719cb58bac9
Chinazaekpere <chinazauchenna@gmail.com>
2024-05-16 12:41:55 +0100

    set back project options to pop up when back project button is clicked

--- js/script.js
+++ js/script.js
@@ -4,15 +4,22 @@ const bookmarkText = document.getElementById("bookmarkTxt");
 const bookmarkImg = document.getElementById("bookmarkImg");
 const enterPledge = document.querySelectorAll(".enterPledgeSection");
 
+const backProject = document.getElementById("backProject");
+const backProjectButton = document.getElementById("backProjectBtn");
+
 document.querySelectorAll("#pledgeOptions").forEach((option) => {
   option.addEventListener("click", () => {
     const index = option.dataset.index;
     pledgeConts.forEach((pledgeCont, idx) => {
-      if (index == idx + 1) {
+      if (index == idx) {
         pledgeCont.style.outlineColor = "#3cb4ac";
-        enterPledge[idx].style.display = "block";
       } else {
         pledgeCont.style.outlineColor = "";
+      }
+
+      if (index == idx + 1) {
+        enterPledge[idx].style.display = "block";
+      } else {
         enterPledge[idx].style.display = "none";
       }
     });
@@ -28,4 +35,8 @@ bookmarkBtn.addEventListener("click", () => {
     bookmarkText.style.color = "";
   }
   bookmarkImg.classList.toggle("bookmarked");
+});
+
+backProjectButton.addEventListener("click", () => {
+  backProject.classList.remove("hidden");
 });
